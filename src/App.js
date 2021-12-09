0f776b4faf9bb066bb179245db5268084dae07f1
ogMav8 <brad.anderson019@gmail.com>
2021-12-09 11:42:54 -0700

    first demo countdown

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+
+import Home from './pages/Home';
+import Header from './components/Header/header';
+import Footer from './components/Footer/footer';
+
 function App() {
   return (
     <Router>
         <div className='flex-column justify-flex-start min-100-vh'>
           <Header/>
           <div className='container'>
-            <Switch> 
-              <Route exact path="/" component={Home} />
+            
+              <Home/>
               {/* <Route exact path="/login" component={Login}/>
               <Route exact path="/signup" component={Signup}/>
               <Route exact path="/profile/:clientName?" component={Profile} />
               <Route exact path="/ticket/:id" component={SingleTicket} />
 
               <Route component={NoMatch} /> */}
-            </Switch>
+            
           </div>
           <Footer/>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const Home = () => {    
+import Countdown from '../components/Countdown/countdown';
+
+const Home = () => {  
+
     return (
         <main>
+            <Countdown/>
             <div className = 'flex-row justify-space-between'>
                 <h1>hello old friend</h1>
             </div>
