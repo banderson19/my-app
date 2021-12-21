f0956d13b08a7ecebf5cfd9cb0ec4487ca125a15
ogMav8 <brad.anderson019@gmail.com>
2021-12-20 21:53:20 -0700

    trying to display lights in shuffle mode

--- src/components/Shuffle/shuffle.js
+++ src/components/Shuffle/shuffle.js
@@ -20,13 +20,17 @@
 import React from 'react';
 
 import redBulb from '../../assets/images/red_ledbulb.jpg';
-import greenBulb from '../../assets/images/green_ledbulb.jpg'
-import whiteBulb from '../../assets/images/white_ledbulb.jpg'
-
+import greenBulb from '../../assets/images/green_ledbulb.jpg';
+import whiteBulb from '../../assets/images/white_ledbulb.jpg';
 
 const Shuffle = () => {
     // turn into object with properties 
-    let lightArr = [
+    let lightArr, patterns, red, green, white
+
+    // red = redBulb
+    // green = greenBulb
+    // white = whiteBulb
+    lightArr = [
         {
             color: "red",
             img: redBulb
@@ -40,6 +44,27 @@ const Shuffle = () => {
             img: whiteBulb
         }
     ]
+    
+    patterns = [
+        {
+            id: 1,
+            pattern: [redBulb, greenBulb, whiteBulb]
+        },
+        {
+            id: 2,
+            pattern: [redBulb, greenBulb, redBulb]
+        },
+        {
+            id: 3,
+            pattern: [redBulb, whiteBulb, redBulb]
+        },
+        {
+            id: 4,
+            pattern: [whiteBulb, greenBulb, whiteBulb]
+        }
+    ]
+
+    console.log(patterns)
 
     const arrayShuffle = (array) =>  {
         for (var i = array.length -1; i > 0; i--){
@@ -51,23 +76,42 @@ const Shuffle = () => {
         return array
     }
     
-    arrayShuffle(lightArr);
-    console.log(lightArr);
-    
+    // arrayShuffle(lightArr);
+    // console.log(lightArr);
+    const result = () => lightArr.map(light => {
+       var color = light.color
+        return color
+    })
+    console.log(lightArr.map(light => ({value : light.color})))
+
 
     return (
         <div>
+            <div className="displayCard">
+                <h6>{patterns[0].id}</h6>
+                {patterns[0].pattern.map((result, i) => {
+                    console.log(result)
+                    return (
+                        <img className="lightCard" src={result}/>
+                    )
+                })}
+            </div>
             {/* <div>
-                {lightArr.map((light, i) => {
-                    // <h4>{light.color}</h4>
-                    <img
-                        src={light.img}
-                        style={{width:"50px" ,length:"50px"}}
-                    />
+                {patterns.map((result, i) => {
+                    console.log('result', result)
+                    console.log(111, result.pattern[0])
+                    {result.pattern.map((data, i) => {
+                        var newdata = JSON.stringify(data)
+                        console.log('newdata', newdata);
+                        return (
+                            <div>
+                            <img className="lightCard" src={data}/>
+                            </div>
+                        )
+                    })}
                 })}
             </div> */}
             
-            <h6>{lightArr[0].img}</h6>
         </div>
     )
 }
