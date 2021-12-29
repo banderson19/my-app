291f9f6b8d1632df0fee72a2b82ba9db4c1c10e8
ogMav8 <brad.anderson019@gmail.com>
2021-12-29 13:44:35 -0700

    stop with shuffle js

--- src/components/Shuffle/shuffle.jsx
+++ src/components/Shuffle/shuffle.jsx
@@ -8,21 +8,6 @@ import whiteBulb from '../../assets/images/white_ledbulb.jpg';
 
 const Shuffle = () => {
         
-        const lightArr= [
-            {
-                color: "red",
-                img: redBulb
-            },
-            {
-                color: "green",
-                img: greenBulb
-            },
-            {
-                color: "white",
-                img: whiteBulb
-            }
-        ]
-        
         const patterns= [
             {
                 id: 1,
@@ -43,61 +28,41 @@ const Shuffle = () => {
         ]
     
 
-        let patternIndex = 0;
 
-        let [index, setIndex] = useState(0)
-        
+        let [index, setIndex] = useState(0);
+        // console.log('index', index)
         const incrementIndex = (index)  => {
-            setIndex(0)
+            setIndex(index+1)
         }
 
         const decrementIndex = (index) => {
-            setIndex(index - 1) 
-        }
-        // setCounter(counter + 1)
-        //             console.log(444, counter)
-        //             if(counter >= patterns.length) {
-        //                 counter = 0;
-        //             }  
-        // const incrementIndex = () => {
-        //     patternIndex ++;
-        //     console.log(patternIndex)
-        // }
-        // console.log(patterns[1])
-        // console.log('index', patternIndex)
-        
-    const arrayShuffle = (array) =>  {
-        for (var i = array.length -1; i > 0; i--){
-            const j = Math.floor(Math.random() *  (i + 1));
-            const  temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-        return array
-    }
+        //     // setIndex(index -1) 
+        } 
+    //     const arrayShuffle = (array) =>  {
+    //     for (var i = array.length -1; i > 0; i--){
+    //         const j = Math.floor(Math.random() *  (i + 1));
+    //         const  temp = array[i];
+    //         array[i] = array[j];
+    //         array[j] = temp;
+    //     }
+    //     console.log('new', array)
+    //     return array
+    // }
     
-    console.log(arrayShuffle(patterns));
-    console.log(lightArr);
-    // const result = () => lightArr.map(light => {
-    //    var color = light.color
-    //     return color
-    // })
-    // console.log(lightArr.map(light => ({value : light.color})))
-
-        
+    // arrayShuffle(patterns);
+    // console.log(arrayShuffle(patterns));
     return (
         <div>
             <div className="displayCard">
                 <button onClick={() => {decrementIndex(index)}}>previous</button>
-                    {patterns[index].pattern.map((data) => {
-                        console.log('data', data)
-                        console.log('index', index)
+                    {patterns[0].pattern.map((data) => {
+                        console.log(data)
                         return (
                             <div key={data.id}>
                                 <img key={data.id} className="lightCard" src={data}/>
                             </div>
                         )
-                    })} 
+                    })}
                 <button onClick={() => {incrementIndex(index)}}>next</button>
             </div>             
         </div>
