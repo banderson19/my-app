788d8162afbe8d1cfd762e8c4c41bc7faea9ac2a
ogMav8 <brad.anderson019@gmail.com>
2022-02-09 15:45:43 -0700

    able to add user to database

---
+++ src/Component_dash/ClientAdd/clientAdd.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class ClientAdd extends Component {
+    constructor(props) {
+        super(props)
+        this.onChangeFirstName = this.onChangeFirstName.bind(this);
+        this.onChangeLastName = this.onChangeLastName.bind(this);
+        this.onChangePhoneNumber = this.onChangePhoneNumber.bind(this);
+        this.onChangeNotes = this.onChangeNotes.bind(this);
+
+        this.onSubmit = this.onSubmit.bind(this);
+        this.state = {
+            firstName: '',
+            lastName: '',
+            phoneNumber: '',
+            notes: ''
+        }
+    }
+    onChangeFirstName(e) {
+        this.setState({ firstName: e.target.value })
+    }
+    onChangeLastName(e) {
+        this.setState({ lastName: e.target.value })
+    }
+    onChangePhoneNumber(e) {
+        this.setState({ phoneNumber: e.target.value })
+    }
+    onChangeNotes(e) {
+        this.setState({ notes: e.target.value })
+    }
+    onSubmit(e) {
+        e.preventDefault()
+        const userObject = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            phoneNumber: this.state.phoneNumber,
+            notes: this.state.notes
+        };
+        axios.post('http://localhost:3001/api/clients', userObject)
+            .then((res) => {
+                console.log(res.data)
+            }).catch((error) => {
+                console.log(error)
+            });
+        this.setState({ firstName: '', lastName: '', phoneNumber: '', notes: ''})
+    }
+
+    render() {
+        return (
+            <div className="wrapper">
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Add first name</label>
+                        <input type="text" value={this.state.firstName} onChange={this.onChangeFirstName} className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Add last name</label>
+                        <input type="text" value={this.state.lastName} onChange={this.onChangeLastName} className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Phone Number</label>
+                        <input type="text" value={this.state.phoneNumber} onChange={this.onChangePhoneNumber} className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Notes</label>
+                        <input type="text" value={this.state.notes} onChange={this.onChangeNotes} className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create User" className="btn btn-success btn-block" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Header from '../component_dash/Header/header.jsx'
+import ClientAdd from '../component_dash/ClientAdd/clientAdd.jsx';
 import ClientList from '../component_dash/ClientList/clientList.jsx';
 import routes from '../routes';
 
@@ -9,6 +10,7 @@ const Dashboard = () => {
     return (
         <div>
             <Header />
+            <ClientAdd/>
             <ClientList/>
         </div>
     )
