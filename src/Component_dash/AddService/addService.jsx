d64601cc8d69bb19cdcac60254e6aee8ca4d8d54
ogMav8 <brad.anderson019@gmail.com>
2022-08-01 18:09:40 -0600

    working on form submit for service and unit

--- src/Component_dash/AddService/addService.jsx
+++ src/Component_dash/AddService/addService.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link, useParams, useLocation } from 'react-router-dom';
+
 import Modal from 'react-bootstrap/Modal'
 
-const AddService = () => {
+
+const AddService = (props) => {
+    // let data = props
     const [show, setShow] = useState(false);
+    const [data, setData] = useState({})
+    setData(props)
+
+    let newUnits = data.units
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    // let { firstName, lastName, phoneNumber, email, street, city, zip, notes, clientAcquired } = data;
+    
+
+    console.log('444', data)
+    console.log(newUnits)
     return (
         <div>
-            <section id='form'>
+            <section id='serviceForm'>
                 <div className="">
                     <button className="btn" onClick={handleShow}>
                         Add Service
@@ -22,44 +36,37 @@ const AddService = () => {
                 keyboard={false}
             >
                 <Modal.Body className="form">
-                    <form class="row g-3">
-                        <div class="col-md-6">
-                            <label for="inputEmail4" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="inputEmail4" />
-                        </div>
-                        <div class="col-md-6">
-                            <label for="inputPassword4" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="inputPassword4" />
-                        </div>
-                        <div class="col-12">
-                            <label for="inputAddress" class="form-label">Unit</label>
-                            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
-                        </div>
-                        <div class="col-md-6">
-                            <label for="inputCity" class="form-label">City</label>
-                            <input type="text" class="form-control" id="inputCity" />
-                        </div>
-                        <div class="col-md-4">
-                            <label for="inputState" class="form-label">State</label>
-                            <select id="inputState" class="form-select">
-                                <option selected>Choose...</option>
-                                <option>...</option>
+                    <form className="row g-3">
+                        {/* <h2>{firstName} {lastName}</h2> */}
+                        <div className="col-12">
+                            <label for="inputUnit" className="form-label">Unit</label>
+                            <select id="inputUnit" className="form-select">
+                                <option selected>Select Unit</option>
+                                {/* {newUnits.map((unit, i) =>                                   
+                                        <option key={unit._id}>{unit.street} {unit.city} Ut {unit.zip}</option>
+                                )} */}
                             </select>
                         </div>
-                        <div class="col-md-2">
-                            <label for="inputZip" class="form-label">Zip</label>
-                            <input type="text" class="form-control" id="inputZip" />
+                        <div className="col-md-6">
+                            <label for="inputDate" className="form-label">Date</label>
+                            <input type="date" className="form-control" id="inputDate" />
                         </div>
-                        <div class="col-12">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" id="gridCheck" />
-                                <label class="form-check-label" for="gridCheck">
-                                    Check me out
-                                </label>
-                            </div>
+                        <div className="col-md-4">
+                            <label for="inputServiceType" className="form-label">Service Type</label>
+                            <select id="inputServiceType" className="form-select">
+                                <option selected>Choose...</option>
+                                <option>Christmas Lights</option>
+                                <option>Tree Arboring</option>
+                                <option>Gutter Clean Out</option>
+                                <option>Other Labor</option>
+                            </select>
                         </div>
-                        <div class="col-12">
-                            <button type="submit" class="btn btn-primary">Sign in</button>
+                        <div className="col-md-2">
+                            <label for="inputCharge" className="form-label">Charge</label>
+                            <input type="text" className="form-control" id="inputCharge" />
+                        </div>
+                        <div className="col-12">
+                            <button type="submit" className="btn btn-primary">Sign in</button>
                         </div>
                     <button variant="secondary" type="button" onClick={handleClose}>
                         Close
